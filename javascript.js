8b5d27cbc25d272989c9714ec21e9b7dc69be3f6
burhan-15 <abroburhan15@gmail.com>
2023-08-14 04:05:23 -0700

    My First javascript Project

---
+++ javascript.js
@@ -0,0 +1,4 @@
+let imageText = document.getElementById(image-text);
+    let textInput = document.getElementById(text-input).innerText;
+    imageText.innerText=textInput
+    console.log(textInput);
